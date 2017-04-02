579b6a7e5b14337663d1c3af6cb6a0f9ddb3d34a
AnthonyQuinn <AnthonyQuinn@172.29.109.36>
2017-04-02 22:07:23 +0100

    Images are now being displayed using jQuery

---
+++ public_html/js/jQuery.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$(function (){
+   $("#submit").on("click",updateXMLFile());
+ 
+    
+    function updateXMLFile() {
+    {
+        alert("Testing @22:14");
+     /**   var $newInput = prompt("Enter new input", "");  **/
+     
+     /**   $(this).text($newInput); **/
+}
+}
+}
+);
+
+
+
--- public_html/js/jsFunctions.js
+++ public_html/js/jsFunctions.js
@@ -3,29 +3,96 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+        
 
-
-function loadXMLDoc() {
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      myFunction(this);
-    }
-  };
-  xmlhttp.open("GET", "cd_catalog.xml", true);
-  xmlhttp.send();
+function loadWorldCupXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this);
+        }
+    };
+    xmlhttp.open("GET", "XML/cd_catalog.xml", true);
+    xmlhttp.send();
 }
 function myFunction(xml) {
-  var i;
-  var xmlDoc = xml.responseXML;
-  var table="<tr><th>Artist</th><th>Title</th></tr>";
-  var x = xmlDoc.getElementsByTagName("CD");
-  for (i = 0; i <x.length; i++) { 
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-    "</td></tr>";
-  }
-  document.getElementById("demo").innerHTML = table;
+    var i;
+    var xmlDoc = xml.responseXML;
+   /** var table = "<table id='sort-table'><thead><tr><th>Sort: <a href='#' class='link-sort-table asc'>Asc</a> <a href='#' class='link-sort-table desc'>Desc</a></th><th> NAME </th><th> MP </th><th> WINS </th><th> DRAWS </th><th> LOSSES </th></tr></thead></table>"; **/
+        var table = "<table><thead><tr><th>Country Flag</th><th> Match </th><th>Country Flag</th><th> Location </th><th> Date </th><th> Result </th></tr></thead></table>";
+
+    var x = xmlDoc.getElementsByTagName("CD");
+    for (i = 0; i < x.length; i++) {
+        table += "<tbody><tr><td>" +
+              x[i].getElementsByTagName("IMAGE1")[0].childNodes[0].nodeValue  +
+                "</td><td>" +
+                x[i].getElementsByTagName("MATCH")[0].childNodes[0].nodeValue +
+                "</td><td>" +
+               x[i].getElementsByTagName("IMAGE2")[0].childNodes[0].nodeValue  +
+                "</td><td>" +
+                x[i].getElementsByTagName("LOCATION")[0].childNodes[0].nodeValue +
+                "</td><td>" +
+                x[i].getElementsByTagName("DATE")[0].childNodes[0].nodeValue +
+                "</td><td contenteditable='true'>" +
+                x[i].getElementsByTagName("RESULT")[0].childNodes[0].nodeValue +
+                "</td></tr></tbody>";
+    }
+    document.getElementById("demo").innerHTML = table;
 }
+
+
+/**
+     var $submit = document.getElementById("submit");
+    
+    $submit.onclick("click",updateFile);
+    
+    
+    function updateFile(){
+        
+        alert("Testing @22:19");
+    }
+   **/ 
+    
+$(function (){
+    
+   $("#submit").on("click",updateWorldCupXMLFile);
+   
+ 
+    
+    function updateWorldCupXMLFile() {
+    {
+        alert("Testing @22:37");
+     
+}
+}
+/**
+function doubleClickAlert(){
+    $( ".WIN" ).click(function() {
+  $( this ).dblclick(alert("Double click called"));
+});
+}
+**/
+/**
+$('.link-sort-table').click(function(e) 
+    var $sort = this;
+    var $table = $('#sort-table');
+    var $rows = $('tbody > tr',$table);
+    $rows.sort(function(a, b){
+        var keyA = $.trim($('td:eq(0)',a).text());
+        var keyB = $.trim($('td:eq(0)',b).text());
+        console.log(keyA, keyB);
+        if($($sort).hasClass('asc')){
+            return (keyA > keyB) ? 1 : -1;
+        } else {
+            return (keyA < keyB) ? 1 : -1;
+        }
+    });
+    $.each($rows, function(index, row){
+      $table.append(row);
+    });
+    e.preventDefault();
+});
+**/
+}
+
+);
