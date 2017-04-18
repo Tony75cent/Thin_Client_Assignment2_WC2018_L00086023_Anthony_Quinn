ed748397ac52a3b8e8c36bd5230328bb6ff33933
AnthonyQuinn <AnthonyQuinn@172.29.109.36>
2017-04-18 19:07:10 +0100

    The table is now reading out using the jQuery AJAX with table headings.

--- public_html/js/jsFunctions.js
+++ public_html/js/jsFunctions.js
@@ -3,20 +3,59 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function LoadFixtures(order) {
-                    xml = loadXMLDoc("cd_catalog.xml");
-                    if (order == "desc") {
-                        xsl = loadXMLDoc("fixtures.xsl");
-                    } else {
-                        xsl = loadXMLDoc("fixtures_1.xsl");
 
-                    }
-                    xsltProcessor = new XSLTProcessor();
-                    xsltProcessor.importStylesheet(xsl);
-                    resultDocument = xsltProcessor.transformToFragment(xml, document);
-                    document.getElementById("matchlist").innerHTML = "";
-                    document.getElementById("matchlist").appendChild(resultDocument);
-                }
+
+$(document).ready(function () { // load xml file using jquery ajax
+    $.ajax({
+        type: "GET",
+        url: "XML/fixtures.xml",
+        dataType: "xml",
+        success: function (xml) {
+            var table = '<table>';
+            table += '<tr><th>Match</th><th>Location</th><th>Date</th><th>Result</th></tr>';
+
+            $(table).addClass('borderClass');
+            $(xml).find('fixtures').each(function () {
+                var match = $(this).find('match').text();
+                var id = $(this).attr('id');
+                var location = $(this).find('location').text();
+                var date = $(this).find('date').text();
+                var result = $(this).find('result').text();
+
+                table += '<tr><td>' + match + '</td><td>' + location + '</td><td>' + date + '</td><td contenteditable=true>' + result + '</td></tr>';
+
+            });
+            table += '</table>';
+            $('#update').html(table);
+        }
+    });
+});
+
+
+
+function LoadFixtures(order) {
+    xml = loadXMLDoc("cd_catalog.xml");
+    if (order == "desc") {
+        xsl = loadXMLDoc("fixtures.xsl");
+    } else {
+        xsl = loadXMLDoc("fixtures.xsl");
+
+    }
+    xsltProcessor = new XSLTProcessor();
+    xsltProcessor.importStylesheet(xsl);
+    resultDocument = xsltProcessor.transformToFragment(xml, document);
+    document.getElementById("matchlist").innerHTML = "";
+    document.getElementById("matchlist").appendChild(resultDocument);
+}
+
+function loadXMLDoc(filename)
+{
+    xhttp = new XMLHttpRequest();
+    xhttp.open("GET", filename, false);
+    xhttp.send("");
+    return xhttp.responseXML;
+}
+
 
 
 function loadWorldCupXMLDoc() {
@@ -26,7 +65,9 @@ function loadWorldCupXMLDoc() {
             myFunction(this);
         }
     };
-    xmlhttp.open("GET", "XML/cd_catalog.xml", true);
+    /** xmlhttp.open("GET", "XML/cd_catalog.xml", true); **/
+    xmlhttp.open("GET", "XML/fixtures.xml", true);
+
     xmlhttp.send();
 }
 function updateFile() {
@@ -36,39 +77,31 @@ function updateFile() {
             myUpdateFunction(this);
         }
     };
-    xmlhttp.open("GET", "XML/cd_catalog.xml", true);
+    xmlhttp.open("GET", "XML/fixtures", true);
     xmlhttp.send();
 }
 
 function myFunction(xml) {
     var i;
     var xmlDoc = xml.responseXML;
-    /** var table = "<table id='sort-table'><thead><tr><th>Sort: <a href='#' class='link-sort-table asc'>Asc</a> <a href='#' class='link-sort-table desc'>Desc</a></th><th> NAME </th><th> MP </th><th> WINS </th><th> DRAWS </th><th> LOSSES </th></tr></thead></table>"; **/
-    var table = "<table><thead><tr><th>Country Flag</th><th> Match </th><th>Country Flag</th><th> Location </th><th> Date </th><th> Result </th><th>  </th></tr></thead></table>";
+    var table = "<table><thead><tr><th> Match </th><th>Location</th><th>Date</th><th>Result</th></tr></thead></table>";
 
-    var x = xmlDoc.getElementsByTagName("FIXTURE");
+    var x = xmlDoc.getElementsByTagName("fixtures");
     for (i = 0; i < x.length; i++) {
         table += "<tbody><tr><td>" +
-                /** x[i].getElementsByTagName("IMAGE1")[0].childNodes[0].nodeValue  + **/
-                x[i].getElementsByTagName("MATCH")[0].childNodes[0].nodeValue +
+                x[i].getElementsByTagName("match")[0].childNodes[0].nodeValue +
                 " </td><td>" +
-                x[i].getElementsByTagName("MATCH")[0].childNodes[0].nodeValue +
+                x[i].getElementsByTagName("location")[0].childNodes[0].nodeValue +
                 "</td><td>" +
-                /** x[i].getElementsByTagName("IMAGE2")[0].childNodes[0].nodeValue + **/
-                x[i].getElementsByTagName("LOCATION")[0].childNodes[0].nodeValue +
-                "</td><td>" +
-                x[i].getElementsByTagName("LOCATION")[0].childNodes[0].nodeValue +
-                "</td><td>" +
-                x[i].getElementsByTagName("DATE")[0].childNodes[0].nodeValue +
+                x[i].getElementsByTagName("date")[0].childNodes[0].nodeValue +
                 "</td><td contenteditable='true' class='edtiable'>" +
-                x[i].getElementsByTagName("RESULT")[0].childNodes[0].nodeValue +
-                "</td><td>" +
-                "<button  class='button' type='button'>UPDATE</button></td></tr></tbody>";
+                x[i].getElementsByTagName("result")[0].childNodes[0].nodeValue +
+                "<!--button  class='button' type='button'>UPDATE</button--></td></tr></tbody>";
     }
     document.getElementById("demo").innerHTML = table;
 }
 function myUpdateFunction(xml) {
-    var ty =100;
+    var ty = 100;
     var i;
     var xmlDoc = xml.responseXML;
     /** var table = "<table id='sort-table'><thead><tr><th>Sort: <a href='#' class='link-sort-table asc'>Asc</a> <a href='#' class='link-sort-table desc'>Desc</a></th><th> NAME </th><th> MP </th><th> WINS </th><th> DRAWS </th><th> LOSSES </th></tr></thead></table>"; **/
@@ -94,24 +127,24 @@ function myUpdateFunction(xml) {
                 "<!--button  class='button' type='button'>submit</button--></td></tr></tbody>";
     }
     document.getElementById("demo").innerHTML = table;
-               
+
 }
-    /**
-    var xmlDoc = xml.responseXML;
-
-    var x = xmlDoc.getElementsByTagName("CD");
-    for (i = 0; i < x.length; i++) {
-         updateNode = "<tbody><tr><td></td><td contenteditable='true' id='edtiable'>" +
-                 x[i].getElementsByTagName("RESULT")[0].childNodes[0].nodeValue + 
-                "</td><td>" +
-                "</td></tr></tbody>";
-    }
-    document.getElementById("demo").innerHTML = table;
-    **/
-/**} 
-
-
 /**
+ var xmlDoc = xml.responseXML;
+ 
+ var x = xmlDoc.getElementsByTagName("CD");
+ for (i = 0; i < x.length; i++) {
+ updateNode = "<tbody><tr><td></td><td contenteditable='true' id='edtiable'>" +
+ x[i].getElementsByTagName("RESULT")[0].childNodes[0].nodeValue + 
+ "</td><td>" +
+ "</td></tr></tbody>";
+ }
+ document.getElementById("demo").innerHTML = table;
+ **/
+/**} 
+ 
+ 
+ /**
  var $submit = document.getElementById("submit");
  
  $submit.onclick("click",updateFile);
@@ -146,19 +179,19 @@ $(function () {
 
 
     }
-  function sortFixtures(){  
-    $.get('cd_catalogue.xml', function(grade){     
-  var officer = $(grade).find('officer');
+    function sortFixtures() {
+        $.get('cd_catalogue.xml', function (grade) {
+            var officer = $(grade).find('officer');
 
-  officer.sort(function(a, b){
-     return (parseInt($(a).attr('id')) - parseInt($(b).attr('rank')));
-  });
+            officer.sort(function (a, b) {
+                return (parseInt($(a).attr('id')) - parseInt($(b).attr('rank')));
+            });
 
-  officer.each(function(i,v){
-    alert($(v).attr('rank'));
-  });
-});
-  }
+            officer.each(function (i, v) {
+                alert($(v).attr('rank'));
+            });
+        });
+    }
     /**
      function doubleClickAlert(){
      $( ".WIN" ).click(function() {
